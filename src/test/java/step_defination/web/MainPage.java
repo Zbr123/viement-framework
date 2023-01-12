1cbc34a722b8077182c280be63f33ce062e8ab40
Zubair Alam <syedzubairalam123@gmail.com>
2023-01-13 02:14:01 +0500

    web fixes e2e

--- src/test/java/step_defination/web/MainPage.java
+++ src/test/java/step_defination/web/MainPage.java
@@ -1,72 +1,75 @@
-//package step_defination.web;
-//
-//import core.utils.AndroidCore.Initializer;
-//import core.utils.BrowerConfig.WebConnector;
-//import io.appium.java_client.remote.MobileCapabilityType;
-//import io.cucumber.java.After;
-//import io.cucumber.java.Before;
-//import io.cucumber.java.en.When;
-//import pages.Page;
-//import java.io.IOException;
-//
-//import static core.utils.AndroidCore.AndroidDriverSetup.androidDriver;
-//import static core.utils.AppiumHook.emulatorRunner.AppiumServerRunner.StartAppiumServer;
-//import static core.utils.BrowerConfig.WebConnector.driver;
-//import static io.appium.java_client.remote.MobileCapabilityType.APP;
-//import static step_defination.Android.AndroidSetup.PORT;
-//
-//public class MainPage extends Page {
-//
-////    @Before("@Web")
-////    public void startTest() throws Exception {
-////        if (myProp.getProperty("platformname").toString().equals("Web")) {
-////            WebConnector connector = new WebConnector();
-////            driver = connector.openBrowser();
-////            driver.get(myProp.getProperty("weburl"));
-////            driver.manage().window().maximize();
-////        }
-////    }
-//
-//    @Before("@initial")
-//    public void startTestAndroid() throws Exception {
-//        if (myProp.getProperty("platformname").toString().equals("Android")) {
+package step_defination.web;
+
+//import core.utils.AndroidCore.AndroidDriverSetup;
+import core.utils.AndroidCore.Initializer;
+import core.utils.BrowerConfig.WebConnector;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import pages.Page;
+import java.io.IOException;
+
+import static core.utils.BrowerConfig.WebConnector.driver;
+
+public class MainPage extends Page {
+
+    @Before("@Web")
+    public void startTest() throws Exception {
+        if (myProp.getProperty("platformname").toString().equals("Web")) {
+            WebConnector connector = new WebConnector();
+            driver = connector.openBrowser();
+            driver.get(myProp.getProperty("weburl"));
+            driver.manage().window().maximize();
+        }
+    }
+
+    @Before("@initial")
+    public void startTestAndroid() throws Exception {
+        if (myProp.getProperty("platformname").toString().equals("Android")) {
 //            PORT = myProp.getProperty("appiumport");
-//            APP = androidconfig.getProperty("androidapk");
+//            APP= androidconfig.getProperty("androidapk");
 //            StartAppiumServer(PORT, APP);
 //            androidDriver(PORT, APP);
-//        }
-//    }
-//
-//    @After("@Web")
-//    public void afterTest() {
-//        if (myProp.getProperty("platformname").toString().equals("Web")) {
-//            if (driver != null) {
-//                driver.quit();
-//            }
-//        }
-//    }
-//
-//    @After("@final")
-//    public void afterTestAndroid() {
-//        if (myProp.getProperty("platformname").toString().equals("Android")) {
-//            //quitAndroidDriver();
-//        }
-//    }
-//
-//    public void iOSDriver() {
-//    }
-//
-//    @When("^\\[Splash screen] Push app in the background")
-//    public void putAppInBackground() throws IOException {
-//        Initializer.moveAppInBackground(); //moveAppInBackgroundFor10sec
-//    }
-//
-//    @When("[Splash screen] Push {string} app in the foreground")
-//    public static void moveAppInForegrounds(String appName) throws InterruptedException {
-//    }
-//
-//    @When("[Main Page] User go the web portal and navigate to the login page")
-//    public static void webBrowser() {
-//        System.out.println("Vimient Web Page");
-//    }
-//}
+        }
+    }
+
+    @After("@Web")
+    public void afterTest() {
+        if (myProp.getProperty("platformname").toString().equals("Web")) {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    @After("@final")
+    public void afterTestAndroid() {
+        if (myProp.getProperty("platformname").toString().equals("Android")) {
+            //quitAndroidDriver();
+        }
+    }
+
+    public void iOSDriver() {
+    }
+
+    @When("^\\[Splash screen] Push app in the background")
+    public void putAppInBackground() throws IOException {
+        Initializer.moveAppInBackground(); //moveAppInBackgroundFor10sec
+    }
+
+    @When("[Splash screen] Push {string} app in the foreground")
+    public static void moveAppInForegrounds(String appName) throws InterruptedException {
+    }
+
+    @When("[Main Page] User go the web portal and navigate to the login page")
+    public static void webBrowser() {
+        System.out.println("Vimient Web Page");
+    }
+
+    @When("[Main Page] Open App")
+    public static void androidOpen() {
+        System.out.println("Vimient Android Open");
+    }
+}
--- src/test/resources/Features/vimient/web/Home.feature
+++
@@ -1,9 +0,0 @@
-Feature: Home
-
-  @vim-003 @Web
-  Scenario:	home page redirection
-  Given  [Main Page] User go the web portal and navigate to the login page
-    When    [Login Page] User enters email srijan-admin@srijan.net
-    And     [Login Page] User enters password Srijan@12345
-    And     [Login Page] User click on login button
-    And     [Admin Page] User click on home button from admin tab
