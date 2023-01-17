bbf3aedfef7ab803dc2fb84be21ec99019c1dc77
Zubair Alam <syedzubairalam123@gmail.com>
2023-01-17 21:17:00 +0500

    fixes

--- src/main/java/pages/android/LoginPage.java
+++ src/main/java/pages/android/LoginPage.java
@@ -9,22 +9,22 @@ import pages.Page;
 //extends BasePage
 public class LoginPage {
 
-    private String emailAddress_Xpath = "//android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.EditText";
-    private String password_Xpath = "//android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[3]/android.widget.EditText";
-    private String loginButton_Xpath = "//android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[4]";
+    private String emailAddress_id = "//android.widget.EditText[@content-desc=\"SignInScreen:EmailInput:email\"]";
+    private String password_id = "//android.widget.EditText[@content-desc=\"SignInScreen:PasswordInput:password\"]";
+    private String loginButton_id = "//android.view.ViewGroup[@content-desc=\"SignInScreen:SignInButton:signinbutton\"]";
 
     public LoginPage(Page page) {
     }
 
      public WebElement getEmailAddress() {
-         return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(emailAddress_Xpath));
+         return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(emailAddress_id));
      }
 
     public WebElement getPassword() {
-        return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(password_Xpath));
+        return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(password_id));
     }
 
     public WebElement getLoginButton() {
-        return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(loginButton_Xpath));
+        return AndroidDriverSetup.getAndroidDriver().findElement(By.xpath(loginButton_id));
     }
 }
