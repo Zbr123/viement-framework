fd58eba5a554e9a6b11f92cb0dac68811fe5f8b0
Syed Zubair Alam <syedzubairalam123@gmail.com>
2023-01-19 00:51:10 +0500

    fixes

--- src/main/java/core/utils/restAssured/BaseTest.java
+++ src/main/java/core/utils/restAssured/BaseTest.java
@@ -1,11 +1,17 @@
 package core.utils.restAssured;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static core.utils.restAssured.RestAssuredUtil.getBaseUrl;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class BaseTest {
-
 
     @BeforeClass
     public void setup() {
@@ -15,10 +21,12 @@ public class BaseTest {
         RestAssuredUtil.setContentType(ContentType.JSON); //Setup Content Type
     }
 
+
     @AfterClass
     public void afterTest() {
         //Reset Values
         RestAssuredUtil.resetBaseURI();
         RestAssuredUtil.resetBasePath();
     }
+
 }
